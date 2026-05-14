021f8e3afaa64366b2cc083963fb6ca90fe61d41
Rahmonberdi Vahabov <rahmonberdivahabov@gmail.com>
2026-05-14 18:26:46 +0100

    chore: bump version to 1.10.1

--- bundle.sh
+++ bundle.sh
@@ -34,9 +34,9 @@ cat > "$CONTENTS/Info.plist" << 'PLIST'
     <key>CFBundleIdentifier</key>
     <string>com.pilotai.daypilot</string>
     <key>CFBundleVersion</key>
-    <string>1.10.0</string>
+    <string>1.10.1</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.10.0</string>
+    <string>1.10.1</string>
     <key>CFBundleExecutable</key>
     <string>DayPilot</string>
     <key>CFBundleIconFile</key>
